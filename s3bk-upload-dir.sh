a0208e87de69dd8fee711821c418ac56acaa0789
Erik Pennebaker <erik@wgp.net>
2019-10-03 12:20:01 -0400

    support for local tar excludes, tweaks

--- s3bk-upload-dir.sh
+++ s3bk-upload-dir.sh
@@ -3,6 +3,8 @@
 . /data/sysadm/_helpers.sh
 . /data/code/s3bk/s3bk-upload-dir-vars.sh
 
+### INCREMENTAL NOT TESTED OR FULLY IMPLEMENTED
+
 INCREMENTAL=0
 
 if [ "$1" == "--single-links-only" ]; then
@@ -21,6 +23,11 @@ fi
 BKDIR=`basename $SRCDIR`
 cd $SRCDIR/.. || error_exit "failed to chdir to $SRCDIR/.."
 
+ADDL_TAR_ARGS=""
+if [ -f .s3bk-additional ]; then
+   . .s3bk-additional
+fi
+
 if [[ $INCREMENTAL != 0 ]]; then
 
    OUTFILE="$S3BK_SCRATCH_DIR/${BKDIR}-inc.tar.xz"
@@ -30,14 +37,15 @@ if [[ $INCREMENTAL != 0 ]]; then
    find $BKDIR -type f -links 1 > $BKDIR/BK_FILE_LIST.inc
 
    set -o pipefail
-   tar cf - -X /data/code/s3bk/s3bk-upload-dir-excludes --exclude-tag=.nobk -T $BKDIR/BK_FILE_LIST.inc | xz -c -T 0 -2 > $OUTFILE
+   tar cf - -X /data/code/s3bk/s3bk-upload-dir-excludes --exclude-tag=.nobk $ADDL_TAR_ARGS -T $BKDIR/BK_FILE_LIST.inc | xz -c -T 0 -2 > $OUTFILE
    TAR_RC=$?
    set +o pipefail
 else
 
    OUTFILE="$S3BK_SCRATCH_DIR/${BKDIR}.tar.xz"
    set -o pipefail
-   tar cf - -X /data/code/s3bk/s3bk-upload-dir-excludes --exclude-tag=.nobk $BKDIR | xz -c -T 0 -2 > $OUTFILE
+   echo tar cf - -X /data/code/s3bk/s3bk-upload-dir-excludes --exclude-tag=.nobk $ADDL_TAR_ARGS $BKDIR \| xz -c -T 0 -2 \> $OUTFILE
+        tar cf - -X /data/code/s3bk/s3bk-upload-dir-excludes --exclude-tag=.nobk $ADDL_TAR_ARGS $BKDIR  | xz -c -T 0 -2  > $OUTFILE
    TAR_RC=$?
    set +o pipefail
 fi
